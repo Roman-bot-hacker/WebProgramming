8ad754b4a94c6805433f3f376caf407df24cec39
Roman Kruk <roman1writer@gmail.com>
2018-10-02 19:43:05 +0300

    Added lab5 files

--- lab2/js/nav_burger.js
+++ lab2/js/nav_burger.js
@@ -4,3 +4,14 @@ function nav_burger_click() {
     }
     else {document.getElementById("nav_menu").style.display = "none";}
 }
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 50.4914036, lng: 30.6002492};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 16, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
